edcd358b9d203a11bf80d1e17e71b9cda77ffe00
bhavadharani216 <bhavadharani216@gmail.com>
2026-02-23 19:01:45 +0530

    added third maximum number solution

---
+++ 414. Third Maximum Number/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        long max = Long.MIN_VALUE;
+        long smax = Long.MIN_VALUE;
+        long tmax = Long.MIN_VALUE;
+
+        for(int num: nums){
+
+            if(num > max){
+                tmax=smax;
+                smax=max;
+                max=num;
+            }
+            else if(num> smax && num!= max){
+                tmax=smax;
+                smax=num;
+            }
+            else if(num>tmax && num!=smax && num!=max){
+                tmax=num;
+            }
+        }
+        return tmax != Long.MIN_VALUE ? (int) tmax : (int) max;
+    }
+}
