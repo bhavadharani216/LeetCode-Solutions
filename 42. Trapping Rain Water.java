4dc90ce4177231099d07708e61c482ca06761264
BHAVADHARANI G <147973983+bhavadharani216@users.noreply.github.com>
2026-08-04 16:41:25 +0530

    Create 42. Trapping Rain Water.java

---
+++ 42. Trapping Rain Water.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int trap(int[] height) {
+        int left = 0;
+        int right = height.length-1;
+        int water =0;
+        int maxleft=height[left];
+        int maxright = height[right];
+
+
+        while(left < right){
+            if(maxleft < maxright){
+                left++;
+                maxleft=Math.max(maxleft , height[left]);
+                water = water + (maxleft- height[left]);
+            }
+            else{
+                right--;
+                maxright =Math.max(maxright, height[right]);
+                water  = water + (maxright - height[right]);
+            }
+        }
+        return water;
+
+        
+    }
+}
